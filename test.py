be0c23b4d66e032a2ee3ffc214aa893a5b91b3e8
Lê Đức Minh Nhật <nhatldm@fo.com>
2025-11-28 09:03:11 +0700

    update code

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 import json
+from utils import get_article_data, build_input_data
+from tqdm import tqdm
+results = "./data/qwen3_infer_27_11_2025.json"
+with open(results, "r", encoding="utf-8") as f:
+    data = json.load(f)
 
-test_path = "./data/test_list_27_11_2025.json"
-suffix = "_".join(test_path.split('_')[-3:])
-print(f"./data/qwen3_infer_{suffix}")
+out_data = data.copy()
+
+for key in tqdm(data.keys()):
+    for item, item_out in zip(data[key], out_data[key]):
+        article_id = item["article_id"]
+        api_data = get_article_data(int(article_id))
+        item_out["data"] = build_input_data(int(article_id))
+        item_out["url"] = api_data["data"]["share_url"] if api_data else None
+
+with open("./data/qwen3_infer_27_11_2025_with_data.json", "w", encoding="utf-8") as out_f:
+    json.dump(out_data, out_f, ensure_ascii=False, indent=4)
